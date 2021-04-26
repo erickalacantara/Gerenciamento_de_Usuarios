36c7e1d0b299b87710b19083e27d90fcaa28c867
Erickalacantara <erickaalcantara1@gmail.com>
2021-04-25 23:42:54 -0300

    uploading only html file

---
+++ index.js
@@ -0,0 +1,8 @@
+var nome = document.querySelector("#exampleInputName"); //Eu estou falando, seleciona pra mim no documento e cade esse ID
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var Email = document.querySelector("#exampleInputEmail1");
+var password = document.querySelector("#exampleInputPassword1");
+var photo = document.querySelector("#exampleInputPhoto");
+// var admin = document.querySelector("#");
